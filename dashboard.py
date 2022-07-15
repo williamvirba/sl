b241603d199f94a9f4f71d8d42ca0a6f405f291f
williamvirba <86878465+williamvirba@users.noreply.github.com>
2022-07-15 09:21:25 +0200

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -10,7 +10,7 @@ import plotly.express as px  # pip install plotly-express
 import streamlit as st  # pip install streamlit
 
 # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
-st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
+st.set_page_config(page_title="Heart rate Dashboard", page_icon=":bar_chart:", layout="wide")
 
 # ---- READ EXCEL ----
 
